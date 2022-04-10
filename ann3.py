978e8009bd67c7dacf7de6296bbf8ae9ed905194
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2022-04-10 18:51:14 -0400

    added sse

--- ann3.py
+++ ann3.py
@@ -10,24 +10,22 @@ data = np.array(data)
 m, n = data.shape
 np.random.shuffle(data)  # shuffle before splitting into dev and training sets
 
-data_dev = data[:m].T
-Y_dev = data_dev[-1]
-X_dev = data_dev[0:n-1]
+data_test = data[:m].T
+Y_test = data_test[-1]
+X_test = data_test[0:n-1]
 
 data_train = data[:m].T
-print(data)
-print(data_train)
 Y_train = data_train[-1]
 X_train = data_train[0:n-1]
 _, m_train = X_train.shape
 
 
 def init_params():
-    W1 = np.random.rand(40, 45) - 0.5
-    b1 = np.random.rand(40, 1) - 0.5
-    W2 = np.random.rand(10, 40) - 0.5
-    b2 = np.random.rand(10, 1) - 0.5
-    return W1, b1, W2, b2
+    weight1 = np.random.rand(40, 45) - 0.5
+    bias1 = np.random.rand(40, 1) - 0.5
+    weight2 = np.random.rand(10, 40) - 0.5
+    bias2 = np.random.rand(10, 1) - 0.5
+    return weight1, bias1, weight2, bias2
 
 
 def ReLU(Z):
@@ -39,11 +37,10 @@ def softmax(Z):
     return A
 
 
-def forward_prop(W1, b1, W2, b2, X):
-    print(X.shape)
-    Z1 = W1.dot(X) + b1
+def forward_prop(weight1, bias1, weight2, bias2, X):
+    Z1 = weight1.dot(X) + bias1
     A1 = ReLU(Z1)
-    Z2 = W2.dot(A1) + b2
+    Z2 = weight2.dot(A1) + bias2
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
 
@@ -59,23 +56,23 @@ def one_hot(Y):
     return one_hot_Y
 
 
-def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+def backward_prop(Z1, A1, Z2, A2, weight1, weight2, X, Y):
     one_hot_Y = one_hot(Y)
     dZ2 = A2 - one_hot_Y
     dW2 = 1 / m * dZ2.dot(A1.T)
     db2 = 1 / m * np.sum(dZ2)
-    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dZ1 = weight2.T.dot(dZ2) * ReLU_deriv(Z1)
     dW1 = 1 / m * dZ1.dot(X.T)
     db1 = 1 / m * np.sum(dZ1)
     return dW1, db1, dW2, db2
 
 
-def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
-    W1 = W1 - alpha * dW1
-    b1 = b1 - alpha * db1
-    W2 = W2 - alpha * dW2
-    b2 = b2 - alpha * db2
-    return W1, b1, W2, b2
+def update_params(weight1, bias1, weight2, bias2, dW1, db1, dW2, db2, alpha):
+    weight1 = weight1 - alpha * dW1
+    bias1 = bias1 - alpha * db1
+    weight2 = weight2 - alpha * dW2
+    bias2 = bias2 - alpha * db2
+    return weight1, bias1, weight2, bias2
 
 
 def get_predictions(A2):
@@ -83,45 +80,55 @@ def get_predictions(A2):
 
 
 def get_accuracy(predictions, Y):
-    print(predictions, Y)
+    print("Predictions vs Actual: ", predictions, Y)
+    print(f"Sum of Square Error: {sum_square_error(predictions, Y)}")
+
     return np.sum(predictions == Y) / Y.size
 
 
 def gradient_descent(X, Y, alpha, iterations):
-    W1, b1, W2, b2 = init_params()
+    weight1, bias1, weight2, bias2 = init_params()
     for i in range(iterations):
-        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-        W1, b1, W2, b2 = update_params(
-            W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        Z1, A1, Z2, A2 = forward_prop(weight1, bias1, weight2, bias2, X)
+        dW1, db1, dW2, db2 = backward_prop(
+            Z1, A1, Z2, A2, weight1, weight2, X, Y)
+        weight1, bias1, weight2, bias2 = update_params(
+            weight1, bias1, weight2, bias2, dW1, db1, dW2, db2, alpha)
         if i % 10 == 0:
-            print("Iteration: ", i)
+            print("Epcoh: ", i)
             predictions = get_predictions(A2)
             print(get_accuracy(predictions, Y))
-    return W1, b1, W2, b2
+    return weight1, bias1, weight2, bias2
 
 
-def make_predictions(X, W1, b1, W2, b2):
-    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+def make_predictions(X, weight1, bias1, weight2, bias2):
+    _, _, _, A2 = forward_prop(weight1, bias1, weight2, bias2, X)
     predictions = get_predictions(A2)
     return predictions
 
 
-def test_prediction(index, W1, b1, W2, b2):
-    current_image = X_train[:, index, None]
-    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+def sum_square_error(prediction, label):
+    return np.sum(np.square(label-prediction))
+
+
+def test_prediction(index, weight1, bias1, weight2, bias2):
+    prediction = make_predictions(
+        X_train[:, index, None], weight1, bias1, weight2, bias2)
     label = Y_train[index]
     print("Prediction: ", prediction)
     print("Label: ", label)
 
 
 if __name__ == '__main__':
-    W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
+    epoch = 50
+    learn_rate = 0.1
+    w1, b1, w2, b2 = gradient_descent(
+        X_train, Y_train, learn_rate, epoch)
 
-    test_prediction(0, W1, b1, W2, b2)
-    test_prediction(1, W1, b1, W2, b2)
-    test_prediction(2, W1, b1, W2, b2)
-    test_prediction(3, W1, b1, W2, b2)
+    test_prediction(0, w1, b1, w2, b2)
+    test_prediction(1, w1, b1, w2, b2)
+    test_prediction(2, w1, b1, w2, b2)
+    test_prediction(3, w1, b1, w2, b2)
 
-    dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
-    get_accuracy(dev_predictions, Y_dev)
+    test_set_predictions = make_predictions(X_test, w1, b1, w2, b2)
+    get_accuracy(test_set_predictions, Y_test)
